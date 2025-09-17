6f1fdeb3f9d9ceacc33f1fe3fa44385748be4b40
nh602 <nicholas.henley@proton.me>
2025-09-17 11:43:43 -0600

    edited timeline elements to take minimal space

--- src/components/project-timeline/project-timeline.tsx
+++ src/components/project-timeline/project-timeline.tsx
@@ -15,39 +15,39 @@ export default async function ProjectTimeline() {
     const projects = await mockProjectAPI.getProjects();
 
     return (
-        <Timeline position="right" sx={{ '& .MuiTimelineItem-root': { minHeight: 'auto', '&::before': { display: 'none' } }}}>
-        {projects.map((project, index) => (
-            <TimelineItem key={project.id}>
-                <TimelineOppositeContent sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
-                    <Typography variant="body2" color="text.secondary" >{project.date}</Typography>
-                </TimelineOppositeContent>
-                <TimelineSeparator sx={{
-                    alignSelf: 'stretch', 
-                    justifyContent: 'center'
-                }}>
-                    <TimelineConnector
-                        sx={{
-                            flex: 1,
-                            visibility: index === 0 ? 'hidden' : 'visible',
-                        }}
-                    />
-                    <TimelineDot />
-                    <TimelineConnector 
-                        sx={{
-                            flex: 1,
-                            visibility: index === projects.length - 1 ? 'hidden' : 'visible',
-                        }}
-                    />
-                </TimelineSeparator>
-                <TimelineContent sx={{ py: 4, display: 'flex', justifyContent: 'flex-start', width: { xs: '100%', sm: 500 }, flex: 0 }}>
-                    <ProjectCard 
-                        title={project.title}
-                        description={project.description}
-                        imageUrl={project.imageUrl}
-                    />
-                </TimelineContent>
-            </TimelineItem>
-        ))}
+        <Timeline position="right" sx={{ alignSelf: "center", '& .MuiTimelineItem-root': { minHeight: 'auto', '&::before': { display: 'none' } } }}>
+            {projects.map((project, index) => (
+                <TimelineItem key={project.id}>
+                    <TimelineOppositeContent sx={{ alignItems: 'center', display: 'flex', flex: "0 0 auto", p: 0, mr: 2 }}>
+                        <Typography variant="body2" color="text.secondary" >{project.date}</Typography>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator sx={{
+                        alignSelf: 'stretch',
+                        justifyContent: 'center'
+                    }}>
+                        <TimelineConnector
+                            sx={{
+                                flex: 1,
+                                visibility: index === 0 ? 'hidden' : 'visible',
+                            }}
+                        />
+                        <TimelineDot />
+                        <TimelineConnector
+                            sx={{
+                                flex: 1,
+                                visibility: index === projects.length - 1 ? 'hidden' : 'visible',
+                            }}
+                        />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ py: 4, display: 'flex', justifyContent: 'flex-start', width: { xs: '100%', sm: 500 }, flex: 0 }}>
+                        <ProjectCard
+                            title={project.title}
+                            description={project.description}
+                            imageUrl={project.imageUrl}
+                        />
+                    </TimelineContent>
+                </TimelineItem>
+            ))}
         </Timeline>
     )
 }
